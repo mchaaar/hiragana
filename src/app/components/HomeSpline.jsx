5c7921dea589942160fa66389dd622011851a6a4
Lain <90577987+Laiinn@users.noreply.github.com>
2024-02-14 23:58:47 +0100

    Modified page layout and moved components to match

---
+++ src/app/components/HomeSpline.jsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Spline from '@splinetool/react-spline';
+
+export default function HomeSpline() {
+    return (
+        <Spline
+            scene="https://prod.spline.design/e9nRoHf7hlsfbG66/scene.splinecode"
+            className="w-full h-auto max-h-[1400px] md:max-h-[700px]"
+        />
+    );
+}
--- src/app/hiragana/page.jsx
+++ src/app/hiragana/page.jsx
@@ -1,10 +1,37 @@
 'use client'
 
+import { useHiragana } from '../components/hooks/useHiragana';
+import DifficultySwitcher from '../components/DifficultySwitcher';
+import SplineViewer from '../components/SplineViewer';
+import HiraganaButtons from '../components/HiraganaButtons';
+import HiraganaListDisplay from '../components/HiraganaListDisplay';
+import HiraganaListSort from '../components/HiraganaListSort';
+import HiraganaList from '../components/HiraganaList';
+import { HiraganaProvider } from '../components/hooks/HiraganaContext';
+
 export default function Hiragana() {
 
+    const hiragana = useHiragana();
+
     return (
-        <div>
-            
-        </div>
+        <HiraganaProvider value={hiragana}>
+            <main className="flex flex-col items-center">
+                <SplineViewer />
+                <HiraganaButtons />
+                <div className='mt-10 flex flex-col items-center'>
+                    <DifficultySwitcher />
+                    <HiraganaListDisplay />
+                </div>
+                {hiragana.listDisplayed && (
+                    <div className='mt-20'>
+                        <h2 className='text-center text-gray-100 text-4xl mb-12'>List of Hiraganas</h2>
+                        {hiragana.listDisplayed && (
+                            <HiraganaListSort />
+                        )}
+                        <HiraganaList />
+                    </div>
+                )}
+            </main>
+        </HiraganaProvider>
     );
 }
