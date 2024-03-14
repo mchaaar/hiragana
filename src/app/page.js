bee5c4276b560b66b235083fee0a4609c8228213
Lain <90577987+Laiinn@users.noreply.github.com>
2024-03-14 22:35:14 +0100

    modified home page to work as before

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,39 @@
-import HomeSpline from './components/HomeSpline';
+'use client'
+
+import { useHiragana } from './components/hooks/useHiragana';
+import DifficultySwitcher from './components/DifficultySwitcher';
+import SplineViewer from './components/SplineViewer';
+import HiraganaButtons from './components/HiraganaButtons';
+import HiraganaListDisplay from './components/HiraganaListDisplay';
+import HiraganaListSort from './components/HiraganaListSort';
+import HiraganaList from './components/HiraganaList';
+import { HiraganaProvider } from './components/hooks/HiraganaContext';
 
 export default function Home(){
+
+  const hiragana = useHiragana();
+
   return (
     <main className="items-center">
-      <div>
-        <HomeSpline />
-      </div>
+      <HiraganaProvider value={hiragana}>
+        <main className="flex flex-col items-center">
+          <SplineViewer />
+          <HiraganaButtons />
+          <div className='mt-10 flex flex-col items-center'>
+            <DifficultySwitcher />
+            <HiraganaListDisplay />
+          </div>
+          {hiragana.listDisplayed && (
+            <div className='mt-20'>
+              <h2 className='text-center text-gray-100 text-4xl mb-12'>List of Hiraganas</h2>
+              {hiragana.listDisplayed && (
+                <HiraganaListSort />
+              )}
+              <HiraganaList />
+            </div>
+          )}
+        </main>
+      </HiraganaProvider>
     </main>
   );
 }
